f2232059fb05d973012eaa9a15a84f987e3a4ed6
HmmmmH <104703575+HmmmmH@users.noreply.github.com>
2022-05-02 22:27:55 +0800

    Add files via upload

--- static/js/qbgzn.js
+++ static/js/qbgzn.js
@@ -97,7 +97,7 @@ saoma.onclick=function(){
 	};
 };
 var pg=document.getElementsByClassName("pg");
-var pgList=["首页","公告","档案","介绍","？？"];
+var pgList=["首页","公告","档案","萌娘百科","鸣谢"];
 var pN=document.getElementById("pN");
 var pG=document.getElementById("pG");
 var left=document.getElementsByClassName("left");
